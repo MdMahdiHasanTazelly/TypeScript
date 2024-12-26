594b3995743d5572ad1329ed77e805825ea4a5ea
Mahdi Hasan Tazelly <mahdihasantazelly@gmail.com>
2024-12-26 10:17:33 +0600

    Learned functions and variables

---
+++ function.ts
@@ -0,0 +1,62 @@
+
+//taking NUMBER type parameter But returning STRING
+function  sum(a: number, b: number){
+    return "hi there!"
+}
+console.log(sum(3, 2));
+
+
+//to get rid of such problem
+
+
+//add RETURN TYPE in a function
+function deduct(a: number, b: number): number{
+    return a-b;
+}
+console.log(`Deduction = `,deduct(4, 1));
+
+
+//RETURN TYPE with ARROW function
+let greet = (s: string): string =>{
+    return(`${s} take greeting`);
+}
+console.log(greet("Hello"));
+
+
+//when function has NO RETURN TYPE
+function error(msg: string): void{
+    console.log(`Error: ${msg}`);
+}
+error("Exception error");
+
+function addTwo(n: number){
+    return n+2;
+}
+console.log(addTwo(3));
+
+
+//normal function
+function signUp(name: string, id: number){
+    console.log(`Name : ${name} ----ID: ${id}`);
+}
+signUp("Jahid", 231);
+
+
+//with arrow function
+let login = (user: string, isPaid: boolean)=>{
+    if(isPaid){
+        return true;
+    }
+    return false;
+}
+
+console.log(login("Rasel", true));
+
+
+//with default parameter
+function group(grpName: string, isAcrive: boolean = true){
+    console.log(`${grpName} is working.`);
+}
+group("Geology");
+
+
---
+++ variableMe.ts
@@ -0,0 +1,18 @@
+//data types
+
+//string
+let greet: string = "hi there!";
+
+console.log(greet.toLowerCase());
+
+//number
+let userId = 2341.5;
+userId.toFixed();
+
+
+//boolean
+let isLoggedIn: boolean = true;
+
+
+
+export {};
